60f5a46a594dd303f265fcee42bf48804ed4b20f
ian-emurgo <ian.burzynski@emurgo.io>
2023-05-14 16:25:57 +0000

    update Applicative instance exercises

--- solutions/Sandbox.hs
+++ solutions/Sandbox.hs
@@ -17,18 +17,49 @@ data UPair a = UPair a a
 instance Functor Maybe' where
   fmap :: (a -> b) -> Maybe' a -> Maybe' b
   fmap _   Nothing' = Nothing'
-  fmap ab (Just' a) = Just' (ab a)
+  fmap f (Just' a) = Just' (f a)
+
+instance Applicative Maybe' where
+  pure :: a -> Maybe' a
+  pure x = Just' x
+
+  (<*>) :: Maybe' (a -> b) -> Maybe' a -> Maybe' b
+  Nothing' <*> _  = Nothing'
+  Just' f  <*> mx = f <$> mx -- fmap f mx
+  -- _ <*> Nothing'      = Nothing'
+  -- Just' f <*> Just' x = Just' $ f x
 
 instance Functor (Either' e) where
   fmap :: (a -> b) -> Either' e a -> Either' e b
   fmap _  (Left'  e) = Left' e
-  fmap ab (Right' a) = Right' $ ab a
+  fmap f  (Right' a) = Right' $ f a
+
+instance Applicative (Either' e) where
+  pure :: a -> Either' e a
+  pure = Right'
+
+  (<*>) :: Either' e (a -> b) -> Either' e a -> Either' e b
+  Left' err <*> _  = Left' err
+  Right' f  <*> ex = f <$> ex
 
 instance Functor (Pair a) where
   fmap :: (b -> c) -> Pair a b -> Pair a c
   fmap f (Pair x y) = Pair x (f y)
 
+instance Monoid a => Applicative (Pair a) where
+  pure  :: b -> Pair a b
+  pure x = Pair mempty x
+
+  (<*>) :: Pair a (b -> c) -> Pair a b -> Pair a c
+  Pair x f <*> Pair x' y = Pair (x <> x') (f y)
+
 instance Functor UPair where
   fmap :: (a -> b) -> UPair a -> UPair b
   fmap f (UPair x y) = UPair (f x) (f y)
 
+instance Applicative UPair where
+  pure :: a -> UPair a
+  pure x = UPair x x
+
+  (<*>) :: UPair (a -> b) -> UPair a -> UPair b
+  UPair f g <*> UPair x y = UPair (f x) (g y)
