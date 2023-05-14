53c2434550ba93f74543ca619e57e74ae69afb6b
ian-emurgo <ian.burzynski@emurgo.io>
2023-05-14 12:30:02 +0000

    update functor practice in Sandbox

--- solutions/Sandbox.hs
+++ solutions/Sandbox.hs
@@ -8,28 +8,27 @@ data Maybe' a = Nothing' | Just' a
 data Either' e a = Left' e | Right' a
   deriving Show
 
+data Pair a b = Pair a b
+  deriving Show
+
+data UPair a = UPair a a
+  deriving Show
+
 instance Functor Maybe' where
   fmap :: (a -> b) -> Maybe' a -> Maybe' b
   fmap _   Nothing' = Nothing'
   fmap ab (Just' a) = Just' (ab a)
-
-instance Applicative Maybe' where
-  pure :: a -> Maybe' a
-  pure a = Just' a
-
-  (<*>) :: Maybe' (a -> b) -> Maybe' a -> Maybe' b
-  Nothing' <*> _   = Nothing'
-  Just' ab <*> ma  = fmap ab ma
 
 instance Functor (Either' e) where
   fmap :: (a -> b) -> Either' e a -> Either' e b
   fmap _  (Left'  e) = Left' e
   fmap ab (Right' a) = Right' $ ab a
 
-instance Applicative (Either' e) where
-  pure :: a -> Either' e a
-  pure a = Right' a
+instance Functor (Pair a) where
+  fmap :: (b -> c) -> Pair a b -> Pair a c
+  fmap f (Pair x y) = Pair x (f y)
 
-  (<*>) :: Either' e (a -> b) -> Either' e a -> Either' e b
-  Left' e   <*> _  = Left' e
-  Right' ab <*> ea = ab <$> ea
+instance Functor UPair where
+  fmap :: (a -> b) -> UPair a -> UPair b
+  fmap f (UPair x y) = UPair (f x) (f y)
+
